07040d2d877d60146b80d8311e093475a7ee2593
DanielKhine <danielrio126@gmail.com>
2020-05-27 18:51:04 +0630

    add user model and login,logout,regitst forms

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Created by DEV-B.
+
+# TODO:
+# Name-REQ
+# Email-REQ
+# User
+# Logo
+# Phone
+# Type
+# Address
+# Location
+# Capacity
+
+# class Shop(models.Model):
+
+#     Name = models.CharField(max_length=64)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+# Created by DEV-B.
+
+def index(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            message = "Invalid credentials."
+            return HttpResponseRedirect(reverse('login'))
+
+    if not request.user.is_authenticated:
+        return render(request, "mainapp/index.html", {"message": None})
+
+    context = {
+        "user": request.user
+    }
+
+    return render(request, "mainapp/index.html", {"message": context})
+
+
+def login_view(request):
+    return render(request, "mainapp/login.html", {"message": None})
+
+
+def register_view(request):
+    context = {"message": None}
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = User.objects.create_user(username, email, password)
+        if user is not None:
+            user.save()
+            context = {"message": "Account creation success!"}
+
+    return render(request, "mainapp/register.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "mainapp/index.html", {"message": "Logged out."})
